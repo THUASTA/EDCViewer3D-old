50fa365912f1a643ae01b5858e32a9cb987238b3
forgrabbit <2133603132@qq.com>
2023-09-18 19:51:59 +0800

    chore: modify Block.cs

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -12,12 +12,15 @@ public class Block : MonoBehaviour
     public Material material3;
     public Material material4;
     public Material material5;
+    public Material material6;
+    public Material material7;
+    public Material material8;
     private List<Material> Materials;
     private int materialCount;
     // Start is called before the first frame update
     void Start()
     {
-        Materials = new List<Material>() { material0, material1, material2, material3, material4, material5 };
+        Materials = new List<Material>() { material0, material1, material2, material3, material4, material5, material6, material7, material8 };
 
         go = gameObject;
         go.GetComponent<MeshRenderer>().material = Materials[0];
@@ -32,14 +35,14 @@ public class Block : MonoBehaviour
 
     void MaterialAdd(Vector3 MaterialPosition)
     {
-        if( materialCount < 5 && transform.position == MaterialPosition )
+        if( materialCount < 8 && transform.position == MaterialPosition )
         {
             materialCount++;
             go.GetComponent<MeshRenderer>().material = Materials[materialCount];
         }
     }
 
-    void MaterialMinus(Vector3 MaterialPosition)
+    void MaterialRemove(Vector3 MaterialPosition)
     {
         if( materialCount > 0 && transform.position == MaterialPosition )
         {
